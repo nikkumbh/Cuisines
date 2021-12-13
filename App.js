5a06ebe5a85ef8ff92d163d6d61a24125f7d4bd9
Prabal Nikkumbh <prabalnikkumbh@PN.Dlink>
2021-12-13 15:16:41 +0530

    added signOut button at proper place

--- App.js
+++ App.js
@@ -1,35 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import HandlingGeoLocation from './screens/HandlingGeoLocation';
-import Test from './screens/ApiAndLocationConnection';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import HandlingGeoLocation from "./screens/HandlingGeoLocation";
+import Test from "./screens/ApiAndLocationConnection";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-    {/* <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>  */}
-    {/* <HandlingGeoLocation/>  */}
-    <Test /> 
-      </>
+      <NavigationContainer>
+        {/* <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator> */}
+        <HomeScreen/>
+      </NavigationContainer>
+      {/* <HandlingGeoLocation /> */}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,105 +1,80 @@
-
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { auth } from '../firebase'
-import { useNavigation } from '@react-navigation/core'
-import HandlingRapidAPI from './HandlingRapidApi'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/core";
+import HandlingRapidAPI from "./HandlingRapidApi";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login");
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const navigation = useNavigation();
-    const handleSignOut = () => {
-        auth
-        .signOut()
-        .then(() => {
-            navigation.replace("Login");
-        })
-        .catch(error => alert(error.message))   
-    }
-
-    return (
-        <>
-        <View style={styles.container}>
-            <View style={styles.emailContainer}>
-            <Text >Welcome Back: {auth.currentUser?.email} </Text>
-            </View>
-            <TouchableOpacity style={styles.button} onPress={handleSignOut}
-            >
-                </TouchableOpacity>
-            <Text style={styles.buttonText}>Sign Out</Text>
+  return (
+    <>
+      <View style={styles.signOutContainer}>
+        <View style={styles.emailContainer}>
+          <Text style={styles.welcomeText}>Welcome: {auth.currentUser?.email} </Text>
         </View>
-                <HandlingRapidAPI />
-                </>
-    )
-}
+        <View style={styles.signOutButtonContainer}>
+        <TouchableOpacity
+          onPress={handleSignOut}
+        >
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
 
-export default HomeScreen
+      <View style={styles.cuisineContainer}></View>
 
+      {/* <HandlingRapidAPI /> */}
+    </>
+  );
+};
+export default HomeScreen;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    emailContainer:{
-        width: '80%',
-        padding: 15,
-        backgroundColor: '#0782F9',
-        borderRadius: 10,
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-    button: {
-        backgroundColor: '#0782F9',
-        // alignSelf: 'flex-end',
-        // position: 'absolute',
-        marginTop: -5,
-        width: '60%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-        marginTop: 40,
-    },
-    buttonText: {
-        left: 145,
-        bottom: 200,
-        // position: 'absolute',
-        // alignSelf: 'flex-end',
-        color: 'grey',
-        fontWeight: '700',
-    },
-})
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center'
-//     },
-//     emailContainer:{
-//         width: '80%',
-//         padding: 15,
-//         backgroundColor: '#0782F9',
-//         borderRadius: 10,
-//         color: 'white',
-//         fontWeight: '700',
-//         fontSize: 16,
-//     },
-//     button: {
-//         backgroundColor: '#0782F9',
-//         left: 145,
-//         bottom: 200,
-//         width: '60%',
-//         padding: 15,
-//         borderRadius: 10,
-//         alignItems: 'center',
-//         marginTop: 40,
-//     },
-//     buttonText: {
-//         left: 145,
-//         bottom: 200,
-//         color: 'white',
-//         fontWeight: '700',
-//     },
-// })
+  signOutContainer: {
+    flex: 1,
+    borderColor: "red",
+    borderWidth: 2,
+    backgroundColor: "#282828",
+  },
+  signOutButtonContainer: {
+    backgroundColor: "#0782F9",
+    width: "20%",
+    padding: 10,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 40,
+    position: "absolute",
+    right: 5,
+    top: 5,
+  },
+  buttonText: {
+    color: "white",
+  },
+  cuisineContainer: {
+    backgroundColor: "#282828",
+    flex: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "green",
+    borderWidth: 2,
+  },
+  emailContainer: {
+    width: "50%",
+    marginTop: 40,
+    padding: 15,
+    backgroundColor: "#0782F9",
+    borderRadius: 10,
+  },
+  welcomeText: {
+      color: "white",
+  }
+});
